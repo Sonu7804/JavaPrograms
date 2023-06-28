914652262464b4f7c46f25b75cdf6987162a1ed7
Sonu Kushwaha <116018711+Sonu7804@users.noreply.github.com>
2023-06-28 23:39:44 +0530

    Add files via upload

---
+++ switchcase.java
@@ -0,0 +1,23 @@
+public class SwitchCase 
+{
+    public static void main(String[] args)
+    {
+    int num = 600;
+
+    switch(num)
+    {
+        case 200: System.out.println("case1");
+        break;
+
+        case 300: System.out.println("case2");
+        break;
+
+        case 100: System.out.println("case3");
+        break;
+
+        default : System.out.println("default");
+
+    } 
+  }
+
+}
