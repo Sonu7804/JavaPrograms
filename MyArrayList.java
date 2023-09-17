33e1339e21edc180c50d085c5991a7b1079f9853
SONU KUSHWAHA <116018711+Sonu7804@users.noreply.github.com>
2023-09-17 20:40:35 +0530

    Add files via upload

---
+++ MyArrayList.java
@@ -0,0 +1,23 @@
+
+
+import java.util.*;
+
+public class MyArrayList // Renamed class to MyArrayList
+{
+    public static void main(String[] args) {
+        
+        List<Integer> al1 = new ArrayList<>(); // Use the List interface and ArrayList class
+
+        al1.add(100);
+        al1.add(200);
+        al1.add(300);
+        System.out.println(al1);
+
+        list<String> all = new MyArrayList<>();
+
+        al2.add("PW Java");
+        al2.add(1);
+
+
+    }
+}
