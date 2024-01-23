504e3db3670be6be94395e9d7de611c03cb578c5
SONU KUSHWAHA <116018711+Sonu7804@users.noreply.github.com>
2024-01-23 23:27:36 +0530

    Add files via upload

---
+++ StringWidth36.java
@@ -0,0 +1,8 @@
+public class StringWidth36 {
+    public static void main(String[] args) {
+        String s1 ="Java by TechnoLamror";
+        System.out.println(s1.endsWith("r"));           //true
+        System.out.println(s1.endsWith("Lamror"));     //trie
+        System.out.println(s1.endsWith("lamror"));    //false
+    }
+}
