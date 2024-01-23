29ccc55e866c9ac8f548f5957a9ae54a193cb4c0
SONU KUSHWAHA <116018711+Sonu7804@users.noreply.github.com>
2024-01-23 23:13:16 +0530

    Add files via upload

---
+++ Compare2String35.java
@@ -0,0 +1,11 @@
+public class Compare2String35 {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        String s3 = "Meklo";
+        String s4 = "Hemlo";
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s1.compareTo(s4));
+    }
+}
